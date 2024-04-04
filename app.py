5ea3dc6e3bd31a32a63d38aa7d526bc6287c5078
Shreyas Hegde <75136935+Shreyas-hacker@users.noreply.github.com>
2024-04-04 17:29:21 +0800

    refractor code

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ device_map = "auto"
 response = None
 model = None
 os.environ["REPLICATE_API_TOKEN"] = "r8_HXkl38P4AwS8wMIQeiM32odzsKNwrir3Ff4BH"
+
 #Shreyas Code
 def initialise_model(model_path,device_map):
     tokenizer = AutoTokenizer.from_pretrained(model_path)
@@ -114,45 +115,6 @@ async def replicate_generation(chat_history):
     )
     output = "".join(output)
     return output
-#Xi Chen initial Code
-# def initialise_model(model_path, device_map):
-
-#     tokenizer = AutoTokenizer.from_pretrained(model_path)
-
-#     model = AutoModelForCausalLM.from_pretrained(
-#         model_path,
-#         device_map=device_map,
-#         #  max_memory={ 0: "2GB", 1: "5GB", 2: "10GB"},
-#         trust_remote_code=False,
-#         revision="main"
-#     )
-
-#     return model, tokenizer
-
-
-
-# async def generate_response(prompt, model, tokenizer):
-
-#     prompt_template = f'''[INST] <<SYS>>" In the list, there are a set of tuples."\
-#         "The first element of the tuple is user1. The other is user 2."\
-#         "Give a grade on how interested user 1 is to user 2."\
-#     <</SYS>>
-#     {prompt}[/INST]
-#     \n\n ### Response:'''
-
-#     # General PROMPT template
-
-#     # print("\n\n*** Generate:")
-#     input_ids = tokenizer(prompt_template, return_tensors="pt").input_ids
-#     if torch.cuda.is_available():
-#         input_ids = input_ids.to("cuda")
-
-#     output = model.generate(inputs=input_ids, temperature=0.7,
-#                             do_sample=True, top_p=0.95, top_k=40, max_new_tokens=512)
-#     response = tokenizer.decode(output[0])
-#     print(response)
-
-#     return response
 
 def respond(message, chat_history):
     bot_message = random.choice(
@@ -166,8 +128,6 @@ async def AI_interest_eval(chat_history):
     print("this is chat history", chat_history)
     stringed_chat = str(chat_history)
     print("string chat: ", stringed_chat)
-    # AI_response = await generate_response(chat_history, model, tokenizer)
-    # AI_response = await generate_response(chat_history, model)
     AI_response = await replicate_generation(chat_history)
     return AI_response
 
@@ -192,9 +152,8 @@ with gr.Blocks() as demo:
         image_button = gr.Button("Extract text from image")
 
     image_button.click(
-        extract_text, inputs=image_input, outputs=image_output)
+        extract_text, inputs=image_input, outputs=image_output
+    )
 
 if __name__ == "__main__":
-    # model, tokenizer = initialise_model(model_path, device_map)
-    # model = initialise_model(model_path, device_map)
     demo.launch(debug=True)
